7c6f3152c03f838b6a43ce16c1f403f49b215e66
Meghana <meghameghana2904@gmail.com>
2024-10-25 13:10:06 +0530

    Moved config.py and templates folder into src directory

---
+++ src/config.py
@@ -0,0 +1,12 @@
+# config.py
+API_KEY = "f82c15f1f07b63f996d491c539c9cc7c"
+CITIES = ["Delhi", "Mumbai", "Chennai", "Bangalore", "Kolkata", "Hyderabad"]
+TEMP_UNIT = "Celsius"  # Can switch between "Celsius" or "Fahrenheit"
+FETCH_INTERVAL = 300  # in seconds, i.e., 5 minutes
+ALERT_THRESHOLD_TEMP = 10  # Configurable
+ALERT_CONSECUTIVE_READINGS = 2  # Number of updates above threshold to trigger alert
+# config.py
+EMAIL_HOST = "smtp.gmail.com"  # e.g., smtp.gmail.com for Gmail
+EMAIL_PORT = 587  # Usually for TLS connections or 465 for SSL
+EMAIL_USER = "meghameghana2904@gmail.com"
+EMAIL_PASS = "wppb lyke ijlx zati"
